5763273c95136370cea13955270503fd74b7f41c
SathsaraniAththanayaka <e18030@eng.pdn.ac.lk>
2022-12-17 11:20:03 +0530

    sign up files added

---
+++ code/lib/screens/sign_up/localWidget/sign_up.dart
@@ -0,0 +1,114 @@
+import 'package:dispensa/widgets/hero_title.dart';
+import 'package:dispensa/widgets/rounded_elevated_button.dart';
+import 'package:dispensa/widgets/rounded_text_formfield.dart';
+import 'package:dispensa/widgets/text_with_textbutton.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+
+import '../../../config.dart';
+import '../../roots.dart';
+import '../sign_up_buttons.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false ,
+      backgroundColor: kBackgroundColor,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: Config.screenWidth! * 0.04),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                
+                HeroTitle(title: 'Sign Up', subtitle: 'Create an account...'),
+                
+                buildTextFormFields(),
+                SignUpButtons(
+                  formKey: _formKey,
+                  emailController: _emailController,
+                  nameController: _nameController,
+                  passwordController: _passwordController,
+                )
+              ],
+            )),
+        ),
+      )
+    );
+  }
+
+  Expanded buildTextFormFields() {
+    return Expanded(
+                flex: 8,
+                child: Column(
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      child: SizedBox(height: Config.screenHeight! * 0.08),
+                  ),
+                    RoundedTextFormField(
+                      controller: _nameController,
+                      hintText: 'Name',
+                      validator: (value){
+                        if(value.toString().length <= 2){
+                          return 'Enter a valid name.';
+                        }
+                        return null;
+                      } ,
+                      ),
+                      SizedBox(height: Config.screenHeight! * 0.02,),
+                    RoundedTextFormField(
+                      controller: _emailController,hintText: 'Email',
+                      validator: (value){
+                        bool _isEmailValid = RegExp(
+                          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                        .hasMatch(value!);
+                        if (!_isEmailValid){
+                          return 'Invalid email.';
+                        }
+                        return null;
+                        
+                      }),
+                      SizedBox(height: Config.screenHeight! * 0.02,),
+                    RoundedTextFormField(
+                      obsecureText: true,
+                      controller: _passwordController,hintText: 'Password',
+                       validator: (value) {
+                        if(value.toString().length < 6){
+                          return 'Password should be longer or equal to 6 characters.';
+                        }
+                        return null;
+                      },
+                    ),
+                      SizedBox(height: Config.screenHeight! * 0.02,),
+                    RoundedTextFormField(
+                      obsecureText: true,
+                      hintText: 'Confirm Password',
+                      validator: (value){
+                        if(value.trim() != _passwordController.text.trim()){
+                          return 'Password does not match!';
+                        }
+
+                         return null;
+                      },)
+                  ],
+                ),
+              );
+  }
+}
+
