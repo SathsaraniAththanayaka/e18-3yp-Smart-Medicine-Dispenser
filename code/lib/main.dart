4e46cb73db711522779205dce71eede90d108423
SathsaraniAththanayaka <e18030@eng.pdn.ac.lk>
2022-12-17 11:21:18 +0530

    main.dart updated

---
+++ code/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:dispensa/screens/roots.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+import 'controllerBindings.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      initialBinding: ControllerBindings(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Root(),
+    );
+  }
+}
+
