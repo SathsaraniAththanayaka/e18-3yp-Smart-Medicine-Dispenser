c8f6a89ceefca2266382da61c7ba9471dc769f08
SathsaraniAththanayaka <e18030@eng.pdn.ac.lk>
2022-12-19 18:14:05 +0530

    home page updated

--- code/lib/screens/home/home.dart
+++ code/lib/screens/home/home.dart
@@ -1,13 +1,16 @@
 import 'package:dispensa/screens/roots.dart';
+import 'package:dispensa/screens/user_type.dart';
 import 'package:dispensa/widgets/hero_title.dart';
 import 'package:dispensa/widgets/rounded_elevated_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 
 import '../../config.dart';
 import '../../controllers/authController.dart';
+import '../../widgets/hero_image.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -21,10 +24,27 @@ class Home extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
             HeroTitle(title: 'Hello ${_authController.displayName.toString().capitalizeString()}', 
-            subtitle: 'Let\'s begin....'),
-            Expanded(flex: 8,child: Container(),),
-            RoundedElevatedButton(title: 'Sign out!',
-            onPressed: ()=> _authController.signOut(),
+            subtitle: 'Let\'s go to the hospital....'),
+            // HeroImage(
+            //         path: 'assets/welcome.png',
+            //         semanticsLabel: 'Login Hero',),
+            Lottie.network("https://assets5.lottiefiles.com/packages/lf20_pkbvwwvg/bienvenida.sifz.4ECFE884.json"),
+            Text('Medicine is not only a science; it is also an art.It does not consist of compounding pills and plasters; it deals with the very processes of life, which must be understood before they may be guided.',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey
+                )),
+            Expanded(flex: 2,child: Container(),),
+            RoundedElevatedButton(title: 'Get started',
+            // onPressed: ()=> _authController.signOut(),
+            onPressed: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const UserType()),
+  );
+            },
             padding: EdgeInsets.symmetric(horizontal: Config.screenWidth! * 0.2),)
           ]);
         }
