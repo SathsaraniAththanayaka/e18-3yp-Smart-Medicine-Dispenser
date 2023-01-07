4dae42117c80ab1dd3e17d09fcb3f1ade8778562
SathsaraniAththanayaka <e18030@eng.pdn.ac.lk>
2023-01-07 15:48:20 +0530

    Medicine history modified

--- code/lib/screens/medicine_history.dart
+++ code/lib/screens/medicine_history.dart
@@ -17,25 +17,85 @@ class _MedicineHistoryState extends State<MedicineHistory> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Ward'),
+        title: Text('Medicine History'),
         centerTitle: true,
       
       ),
-      body: SingleChildScrollView(child: StreamBuilder(
-          stream: FirebaseFirestore.instance
-              .collection('Patients')
-              .doc(widget.documentId) //ID OF DOCUMENT
-              .snapshots(),
-        builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return new CircularProgressIndicator();
-        }
-        var document = snapshot.data;
-        return new Text(document!["FirstName"]);
+     body: Center(
+      child: Timeline(),
+     ),
+      );
+    
      }
-  ),
-),
+  
+
       
+}
+
+class Timeline extends StatefulWidget {
+  const Timeline({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<Timeline> createState() => _TimelineState();
+}
+
+class _TimelineState extends State<Timeline> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:ListView.builder(
+        itemCount: 1,
+        itemBuilder: ((context, index){
+          return Container(
+            child: Row(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Container(
+                      width: 2,
+                      height: 50,
+                      color: Colors.black,
+
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 5,right: 5),
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(50)
+                      ),
+                      child: Icon(Icons.done,color:Colors.white,),
+                    )
+                  ],
+                ),
+                Expanded(child:Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [BoxShadow(
+                      blurRadius: 10,
+                      color: Colors.black26
+                    )]
+                  ),
+                  height: 100,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text("Date: 2022-12-19 ",style:TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+                        Text("Meal: Morning",style: TextStyle(fontSize: 16)),
+                        Text("Prescription",style: TextStyle(fontSize: 16)),
+                        Text("    Paracetamol: 2",style: TextStyle(fontSize: 16))
+                      ],
+                    ),
+                  ),
+                ))
+              ],
+            )
+          );
+        })),
     );
   }
 }
