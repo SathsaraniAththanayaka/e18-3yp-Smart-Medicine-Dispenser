965da64188e22f74c2ad1349ab131ebedc2a6836
SathsaraniAththanayaka <e18030@eng.pdn.ac.lk>
2023-01-07 15:49:43 +0530

    patient profile updated

--- code/lib/screens/patient_profile.dart
+++ code/lib/screens/patient_profile.dart
@@ -26,6 +26,7 @@ class _PatientProfileState extends State<PatientProfile> {
       ),
       
       body: SingleChildScrollView(child: StreamBuilder(
+        
           stream: FirebaseFirestore.instance
               .collection('Patients')
               .doc(widget.documentId) //ID OF DOCUMENT
@@ -39,93 +40,216 @@ class _PatientProfileState extends State<PatientProfile> {
         
         
         
-        return  Center(
-          child: Column(
-            children: [
-              SizedBox(height: 10,),
-              Text('Name:- '+document!["FirstName"]+" "+document["LastName"]+"\n",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey
+        return  Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          
+          children: [
+            SizedBox(height: 25,),
+            Container(
+                height:120,
+                child: Image(image: AssetImage("assets/patientProf.png"),
+                fit: BoxFit.contain,
                 ),
-                textAlign: TextAlign.right,),
-
-              Text('Date of Birth:- '+document["BirthDate"]+"\n",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey
-                ),
-                textAlign: TextAlign.start
               ),
 
-              Text('Gender:- '+document["Gender"]+"\n",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey
-                ),
-                textAlign: TextAlign.start
-              ),
-
-              Text('Date of admittance:- '+document["DateOfAdmittance"]+"\n",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey
-                ),
-                textAlign: TextAlign.start
-              ),
-              
-              Text('Diagnosis:- '+document["Diagnosis"]+"\n",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey
-                ),
-                textAlign: TextAlign.start
-              ),
-              
-              Text('Checked By:- '+document["CheckedBy"]+"\n",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey
-                ),
-                textAlign: TextAlign.start
-              ),
-              
-              Text('Bed No:- '+document["BedNo"]+"\n",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey
-                ),
-                textAlign: TextAlign.start
-              ),
-              Text('Guardian\'s contact No. :- '+document["GuardianConNo"]+"\n",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey
-                ),
-                textAlign: TextAlign.start
-              ),
-              Text('Status:- '+document["Status"]+"\n",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey
-                ),
-                textAlign: TextAlign.right
-              ),
-              
-              
-              
-              
-            ],
+            SizedBox(height: 25,),
+            const Divider(
+            color: Colors.purple,
+            
+            thickness: 5,
+            indent: 5,
+            endIndent: 5,
           ),
+             Container(
+              decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+              ),
+               child: Text(' Personal details',
+                  style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black
+                  ),
+                  textAlign: TextAlign.start),
+             ),
+
+            const Divider(
+            color: Colors.purple,
+            
+            thickness: 5,
+            indent: 5,
+            endIndent: 5,
+          ),
+
+          SizedBox(height: 15,),
+            Text('    Name: '+document!["FirstName"]+" "+document["LastName"]+"\n",
+            style: TextStyle(
+              fontSize: 18,
+              
+              color: Colors.blueGrey
+              ),
+              textAlign: TextAlign.start),
+
+
+            
+            
+          
+            Text('    Date of Birth: '+document["BirthDate"]+"\n",
+            style: TextStyle(
+              fontSize: 18,
+              // fontWeight: FontWeight.bold,
+              color: Colors.blueGrey
+              ),
+              textAlign: TextAlign.start
+            ),
+           
+
+          
+
+          
+
+            Text('    Gender: '+document["Gender"]+"\n",
+            style: TextStyle(
+              fontSize: 18,
+              // fontWeight: FontWeight.bold,
+              color: Colors.blueGrey
+              ),
+              textAlign: TextAlign.start
+            ),
+
+            Text('    Guardian\'s contact No. : '+document["GuardianConNo"]+"\n",
+            style: TextStyle(
+              fontSize: 18,
+              // fontWeight: FontWeight.bold,
+              color: Colors.blueGrey
+              ),
+              textAlign: TextAlign.start
+            ),
+
+
+            const Divider(
+            color: Colors.lightGreen,
+            
+            thickness: 5,
+            indent: 5,
+            endIndent: 5,
+          ),
+
+            Container(
+              decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+              ),
+               child: Text(' Admittance details',
+                  style: TextStyle(
+                  fontSize: 19,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black
+                  ),
+                  textAlign: TextAlign.start),
+             ),
+
+            const Divider(
+            color: Colors.lightGreen,
+            
+            thickness: 5,
+            indent: 5,
+            endIndent: 5,
+          ),
+
+          SizedBox(height: 15,),
+
+
+
+            Text('    Date of admittance: '+document["DateOfAdmittance"]+"\n",
+            style: TextStyle(
+              fontSize: 18,
+              // fontWeight: FontWeight.bold,
+              color: Colors.blueGrey
+              ),
+              textAlign: TextAlign.start
+            ),
+            
+          
+
+            Text('    Diagnosis: '+document["Diagnosis"]+"\n",
+            style: TextStyle(
+              fontSize: 18,
+              // fontWeight: FontWeight.bold,
+              color: Colors.blueGrey
+              ),
+              textAlign: TextAlign.start
+            ),
+            
+            
+
+            Text('    Checked By: '+document["CheckedBy"]+"\n",
+            style: TextStyle(
+              fontSize: 18,
+              // fontWeight: FontWeight.bold,
+              color: Colors.blueGrey
+              ),
+              textAlign: TextAlign.start
+            ),
+            
+             const Divider(
+            color: Colors.pink,
+            
+            thickness: 5,
+            indent: 5,
+            endIndent: 5,
+          ),
+
+            Container(
+              decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+              ),
+               child: Text(' Current status',
+                  style: TextStyle(
+                  fontSize: 19,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black
+                  ),
+                  textAlign: TextAlign.start),
+             ),
+
+            const Divider(
+            color: Colors.pink,
+            
+            thickness: 5,
+            indent: 5,
+            endIndent: 5,
+          ),
+
+          SizedBox(height: 15,),
+
+
+
+            Text('    Bed No: '+document["BedNo"]+"\n",
+            style: TextStyle(
+              fontSize: 18,
+              // fontWeight: FontWeight.bold,
+              color: Colors.blueGrey
+              ),
+              textAlign: TextAlign.start
+            ),
+
+            
+          
+
+            Text('    Status: '+document["Status"]+"\n",
+            style: TextStyle(
+              fontSize: 18,
+              // fontWeight: FontWeight.bold,
+              color: Colors.blueGrey
+              ),
+              textAlign: TextAlign.start
+            ),
+            
+            
+            
+            
+          ],
         );
         
          }
